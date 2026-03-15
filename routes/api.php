f1ff835f1bd213e1b9754ff0f6fde05a9663f594
Victory Okon <victoryozitech1@gmail.com>
2026-03-15 21:36:00 +0100

    updates -- victory

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
