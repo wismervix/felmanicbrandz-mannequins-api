67d4334f040f7b0bfcc1d9b5446182b01b629479
Victory Wisdom Okon <etangdgm001@gmail.com>
2026-04-22 12:00:02 +0100

    updates

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+'paths' => ['api/*'],
+
+'allowed_methods' => ['*'],
+
+'allowed_origins' => [
+'https://felmanicbrandz.com',
+'https://www.felmanicbrandz.com'
+],
+
+'allowed_headers' => ['*'],
+
+'supports_credentials' => true,
