5089c8209863544acf88673268101e4463d1ed99
Victory Wisdom Okon <etangdgm001@gmail.com>
2026-04-22 15:34:30 +0100

    updates

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -32,13 +32,16 @@ class UserController extends Controller
     )]
     public function index()
     {
-        try {
-            return User::all();
-        } catch (\Exception $e) {
-            return response()->json([
-                'error' => $e->getMessage()
-            ], 500);
-        }
+        return response()->json([
+            'test' => 'users route working'
+        ]);
+        // try {
+        //     return User::all();
+        // } catch (\Exception $e) {
+        //     return response()->json([
+        //         'error' => $e->getMessage()
+        //     ], 500);
+        // }
         // return response()->json([
         //     'users' => User::all(),
         // ]);
