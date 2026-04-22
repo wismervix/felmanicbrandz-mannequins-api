8115449eb29bc10b10742b8c84ad8aecce7a14a6
Victory Wisdom Okon <etangdgm001@gmail.com>
2026-04-22 16:02:36 +0100

    updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,11 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
-
-        $middleware->group('api', [
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
