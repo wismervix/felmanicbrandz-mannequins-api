9b3e3b5c821e13cd449762fac621468cd51a88a8
Victory Wisdom Okon <etangdgm001@gmail.com>
2026-04-24 21:15:48 +0100

    updates

--- app/Services/CloudinaryService.php
+++ app/Services/CloudinaryService.php
@@ -11,24 +11,13 @@ class CloudinaryService
 
     public function __construct()
     {
-        // $this->cloudinary = new Cloudinary();
+        $url = config('cloudinary.cloud_url');
 
-        $this->cloudinary = new Cloudinary([
-            'cloud' => [
-                'url' => config('cloudinary.cloud_url'),
-            ],
-        ]);
+        if (!$url) {
+            throw new \Exception('Cloudinary config missing');
+        }
 
-    //     $this->cloudinary = new Cloudinary([
-    //         'cloud' => [
-    //             'cloud_name' => config('services.cloudinary.cloud_name'),
-    //             'api_key' => config('services.cloudinary.api_key'),
-    //             'api_secret' => config('services.cloudinary.api_secret'),
-    //         ],
-    //         'url' => [
-    //             'secure' => true,
-    //         ],
-    //     ]);
+        $this->cloudinary = new Cloudinary($url);
     }
 
     public function upload($file, $folder = 'general')
